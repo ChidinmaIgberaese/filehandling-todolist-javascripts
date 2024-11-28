21f556fc78581e2f0929df4d99915eb144dc99cb
Chidinma Igberaese <blessingchidinma88@gmail.com>
2024-11-28 09:47:54 +0100

    updated config.json

--- backend/config.js
+++ backend/config.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',  // your MySQL username
-  password: 'yourpassword',  // your MySQL password
-  database: 'todoApp'
+  host: process.env.DB_HOST, // e.g., 'localhost'
+  user: process.env.DB_USER, // e.g., 'root'
+  password: process.env.DB_PASS, // your MySQL password
+  database: process.env.DB_NAME, // the name of your database
+  waitForConnections: true,
+  connectionLimit: 10, // Maximum number of connections in the pool
+  queueLimit: 0, // Unlimited queue length
 });
 
 module.exports = pool.promise();
